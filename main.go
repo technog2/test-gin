1eb789b9b100a52f28795b40f98e57ea800dba6e
sagharizadeh <muhusaghari@gmail.com>
2025-03-17 17:01:35 +0330

    some changes for routing and controllers

--- main.go
+++ main.go
@@ -3,10 +3,36 @@ package main
 import (
 	"net/http"
 
-	// "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 
-	"github.com/technog2/test-gin/router"
+	// "github.com/technog2/test-gin/router"
+
+	// "github.com/technog2/test-gin/controllers/index"
+	// "github.com/technog2/test-gin/controllers/ping"
+	// "github.com/technog2/test-gin/controllers/get-user"
+	// "github.com/technog2/test-gin/controllers/post-user"
+	"github.com/technog2/test-gin/controllers"
 )
+
+func InitRouter() *gin.Engine {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	apiv1 := r.Group("/api/v1")
+
+	// apiv1.Use(jwt.JWT())
+
+	{
+		apiv1.GET("/", controller.Index)
+		apiv1.GET("/ping", controller.Ping)
+		apiv1.GET("/user/:name", controller.GetUser)
+		apiv1.POST("/user", controller.PostUser)
+	}
+
+	return r
+}
 
 func main() {
 	// router := gin.Default()
